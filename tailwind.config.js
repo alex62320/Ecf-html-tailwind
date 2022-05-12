d24fcb5afbb64aa8ad148cf139b51af3b6e68822
alex62320 <alex.62320@live.fr>
2022-05-12 11:21:24 +0200

    Ajout de l'ecf
    ajout ecf fini

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+      'white': '#ffffff',
+      'red' : '#de1738',
+      'slate' : '#334155',
+      'indigo1': '#4338ca',
+      'indigo2': '#3730a3'
+    },
+    fontFamily: {
+      sans: ['"Bebas Neue"', 'cursive'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
